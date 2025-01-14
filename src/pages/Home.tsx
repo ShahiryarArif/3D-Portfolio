8c323fee4eae67a1380782b20b424de639d78498
Your Name <shahiryararif@gmail.com>
2025-01-14 20:18:53 +0500

    React Router and basic setup

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+export const Home: React.FC = () => {
+    return (
+      <section className="w-full h-screen relative">
+            {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+                Popup
+            </div> */}
+            <canvas className="w-full h-full" id="canvas">
+                
+            </canvas>
+      </section>
+      );
+  }
---
+++ src/pages/Projects.tsx
@@ -0,0 +1,11 @@
+export const Projects: React.FC = () => {
+    return (
+      <div>
+          <h1>Projects</h1>
+          <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+              consectetur, orci non ultricies scelerisque
+          </p>
+      </div>
+      );
+  }
