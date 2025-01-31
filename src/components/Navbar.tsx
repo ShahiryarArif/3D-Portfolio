8891fc77acb99be9abfcd18c009c1265d1069912
Your Name <shahiryararif@gmail.com>
2025-01-31 20:48:10 +0500

    Fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ export const Navbar = () => {
             <NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shodow-md">
                 <p className="blue-gradient_text">SA</p>
             </NavLink>
-            <nav className="flex text-lg gap-7">
+            <nav className="flex text-lg gap-7 font-medium">
                 <NavLink to="/" className={({isActive}) => isActive ? "text-blue-500" : "text-black"}>Home</NavLink>
                 <NavLink to="/about" className={({isActive}) => isActive ? "text-blue-500" : "text-black"}>About</NavLink>
                 <NavLink to="/projects" className={({isActive}) => isActive ? "text-blue-500" : "text-black"}>Projects</NavLink>
